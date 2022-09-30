31fb71435338b7c8f114cef36f3c1ce38f060f25
0xol <86998622+0xol@users.noreply.github.com>
2022-09-30 23:23:31 +1000

    printing works

--- src/init.c
+++ src/init.c
@@ -3,6 +3,8 @@
 EFI_STATUS EFI_MAIN(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SysTable) {
     
     SysTable->ConOut->Reset(SysTable->ConOut, EFI_TRUE);
+    SysTable->ConOut->EnableCursor(SysTable->ConOut, EFI_TRUE);
+    SysTable->ConOut->OutputString(SysTable->ConOut, L"VIZBOOT\r\n");
     
     for(;;);
 }
